0ad9e2098735d5a9b86969389436824a7d56ef6d
SekoVper <damehlivingstone1@gmail.com>
2022-11-09 15:14:12 +0100

    Add Testing for removing an item

--- src/__test__/script.test.js
+++ src/__test__/script.test.js
@@ -1,39 +1,57 @@
-import { add, tasks } from '../script.js';
+import { add, tasks, remove } from '../script.js';
 
-describe('Adding task to localstorage', () => {
+describe('Adding task to localStorage', () => {
   test('Test for add function', () => {
     const task = {
       id: 1,
       index: 1,
-      descrption: 'good',
+      description: 'good',
       complete: false,
     };
     add(task);
     expect(tasks.length).toBe(1);
-    expect(tasks[0].descrption).toBe('good');
+    expect(tasks[0].description).toBe('good');
   });
 
   test('Test for add function', () => {
     const task = {
       id: 2,
       index: 2,
-      descrption: 'bad',
+      description: 'bad',
       complete: false,
     };
     add(task);
     expect(tasks.length).toBe(2);
-    expect(tasks[1].descrption).toBe('bad');
+    expect(tasks[1].description).toBe('bad');
   });
 
   test('Test for add function', () => {
     const task = {
       id: 3,
       index: 3,
-      descrption: 'happy',
+      description: 'happy',
       complete: false,
     };
     add(task);
     expect(tasks.length).toBe(3);
-    expect(tasks[2].descrption).toBe('happy');
+    expect(tasks[2].description).toBe('happy');
+  });
+});
+
+describe('Deleting task from localStorage', () => {
+  test('Test for delete function', () => {
+    const { id } = tasks[2];
+    remove(id);
+    expect(tasks.length).toBe(2);
+  });
+  test('Test for delete function', () => {
+    const { id } = tasks[1];
+    remove(id);
+    expect(tasks.length).toBe(1);
+  });
+  test('Test for delete function', () => {
+    const { id } = tasks[0];
+    remove(id);
+    expect(tasks.length).toBe(0);
   });
 });
